14e67666b9aec5035f0e9ae5ccfb7d1ef3d77108
Caroline Carvalho <44538828+Caroline-Carvalho@users.noreply.github.com>
2019-08-14 17:30:07 -0300

    Add files via upload

---
+++ 1001_Extremamente_Basico.py
@@ -0,0 +1,12 @@
+'''
+URI Online Judge | 1001 - Extremamente Básico
+@author Caroline Carvalho
+'''
+
+import sys
+
+a = int(sys.stdin.readline())
+b = int(sys.stdin.readline())
+x = (a + b)
+
+print('X =', x)
