22331489d7d1945c32db6924845c37bf8638f52f
Caroline Carvalho <44538828+Caroline-Carvalho@users.noreply.github.com>
2019-08-15 13:51:41 -0300

    Add files via upload

---
+++ Python/1003_Soma_Simples.py
@@ -0,0 +1,13 @@
+'''
+URI Online Judge | 1003 - Soma Simples
+@author Caroline Carvalho
+'''
+
+import sys
+
+a = int(sys.stdin.readline())
+b = int(sys.stdin.readline())
+
+soma = int(a + b)
+
+print('SOMA = %d' %soma) 
