b15bc00f57855863d784b33f6e5f7bf0fb2d8279
Caroline Carvalho <44538828+Caroline-Carvalho@users.noreply.github.com>
2019-08-15 17:08:16 -0300

    Add files via upload

---
+++ Python/1004_Produto_Simples.py
@@ -0,0 +1,13 @@
+'''
+URI Online Judge | 1004 - Produto Simples
+@author Caroline Carvalho
+'''
+
+import sys
+
+x = int(sys.stdin.readline())
+y = int(sys.stdin.readline())
+
+prod = int(x * y)
+
+print('PROD = %d' %prod)
