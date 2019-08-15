a107c5f04cf7e221072cedd01810656ba8ff6895
Caroline Carvalho <44538828+Caroline-Carvalho@users.noreply.github.com>
2019-08-15 17:10:15 -0300

    Add files via upload

---
+++ Python/1005_Media_1.py
@@ -0,0 +1,13 @@
+'''
+URI Online Judge | 1005 - Média 1
+@author Caroline Carvalho
+'''
+
+import sys
+
+a = float(sys.stdin.readline())
+b = float(sys.stdin.readline())
+
+media = ((3.5 * a) + (7.5 * b)) / 11
+
+print('MEDIA = %.5f' %media)
